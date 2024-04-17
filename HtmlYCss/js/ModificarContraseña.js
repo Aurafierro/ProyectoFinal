e63cbc57d00ae25be6ae5de754b788c240eac4ba
Kevin Camilo <kevincamilo1211@gmail.com>
2024-04-17 17:44:30 -0500

    Se agregó el modificar contraseña

---
+++ HtmlYCss/js/ModificarContraseña.js
@@ -0,0 +1,17 @@
+document.getElementById("passwordForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var newPassword = document.getElementById("newPassword").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    
+    if (newPassword !== confirmPassword) {
+      document.getElementById("message").textContent = "Las contraseñas no coinciden.";
+    } else {
+      // Simulamos una alerta para indicar que la contraseña se ha modificado
+      alert("¡Contraseña modificada exitosamente!");
+      // Limpia los campos del formulario
+      document.getElementById("passwordForm").reset();
+      // Restablece el mensaje de error, si lo hubiera
+      document.getElementById("message").textContent = "";
+    }
+  });
+  
