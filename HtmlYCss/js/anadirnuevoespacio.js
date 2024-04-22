37058843982e377905fc6d3ca1518d55d511123f
kevin muñoz <kevincamilo1211@gmail.com>
2024-04-21 21:24:08 -0500

    se agrego el añadir nuevo espacio

---
+++ HtmlYCss/js/anadirnuevoespacio.js
@@ -0,0 +1,24 @@
+document.getElementById('agregarIcono').addEventListener('click', function() {
+    document.getElementById('imagenEvento').click();
+  });
+  
+  document.getElementById('imagenEvento').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      document.getElementById('imagenSeleccionada').setAttribute('src', e.target.result);
+      document.getElementById('imagenSeleccionada').style.display = 'block';
+    }
+    reader.readAsDataURL(file);
+  });
+
+  document.getElementById('nombreEvento').addEventListener('click', function() {
+    this.placeholder = '';
+  });
+  
+  document.getElementById('nombreEvento').addEventListener('blur', function() {
+    if (this.value === '') {
+      this.placeholder = 'Escribe el nombre del evento...';
+    }
+  });
+  
