2d94065a41cf4f1441bc44abf579126e26566a54
Sebastian Penna <sebastianpennaariss@gmail.com>
2024-07-11 17:40:16 -0500

    se hizo cambios en recuperar contraseña

--- HtmlYCss/js/RecuperarContraseña.js
+++ HtmlYCss/js/RecuperarContraseña.js
@@ -1,23 +1,20 @@
-// Función para cambiar el orden de los elementos dependiendo del tamaño de la pantalla
 function rearrangeElements() {
     var container = document.querySelector('.container');
     var formContainer = document.getElementById('form-container');
     var imageContainer = document.getElementById('image-container');
 
-    if (window.innerWidth > 768) {
-        container.style.flexDirection = 'row'; // Cambia a disposición horizontal
-        formContainer.style.order = '2';
-        imageContainer.style.order = '1';
-    } else {
-        container.style.flexDirection = 'column'; // Cambia a disposición vertical
-        formContainer.style.order = '';
-        imageContainer.style.order = '';
-    }
+    formContainer.style.transform = 'translateX(80%)';
+    imageContainer.style.transform = 'translateX(-125%)';
+
+    setTimeout(() => {
+        formContainer.style.transform = 'translateX(1)';
+        imageContainer.style.transform = 'translateX(1)';
+    }, 800);
 }
 
 // Llama a la función después de 1 segundo de que la página haya cargado
 window.onload = function() {
-    setTimeout(rearrangeElements, 1000); // Espera 1000 milisegundos (1 segundo)
+    setTimeout(rearrangeElements, 1100);
 };
 
 // Llama a la función al redimensionar la ventana
