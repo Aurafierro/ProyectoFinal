b70cacda7988675e326c9ebef507de04299ac968
Aurafierro <auramariafierrofierro@gmail.com>
2024-07-18 09:48:22 -0500

    crear cuenta js

--- HtmlYCss/js/crearCuenta.js
+++ HtmlYCss/js/crearCuenta.js
@@ -1,4 +1,75 @@
-// Función para validar el número de documento
+var url = "http://localhost:8080/api/v1/user/";
+
+
+
+
+function crearCuenta() {
+
+
+    let formData = {
+      "tipo_documento": document.getElementById("tipo_documento").value,
+      "numero_documento": document.getElementById("numero_documento").value,
+      "nombre_completo": document.getElementById("nombre_completo").value,
+      "telefono": document.getElementById("telefono").value,
+      "correo": document.getElementById("correo").value,
+      "contrasena": document.getElementById("contrasena").value,
+      "confirmar_contrasena": document.getElementById("confirmar_contrasena").value,
+      "rol": document.getElementById("rol").value
+
+  
+    };
+  
+    let camposValidos = true;
+    let camposRequeridos = [
+        "tipo_documento",
+        "numero_documento",
+        "nombre_completo",
+        "telefono",
+        "correo",
+        "contrasena",
+        "confirmar_contrasena",
+        "rol"
+
+    ];
+  
+    camposRequeridos.forEach(function(campo) {
+        let valorCampo = document.getElementById(campo).value.trim();
+        if (valorCampo === "") {
+            camposValidos = false;
+            return false; // Terminar la iteración si se encuentra un campo vacío
+        }
+    });
+  
+    if (camposValidos) {
+        $.ajax({
+            url: url,
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(formData),
+            success: function (result) {
+                Swal.fire({
+                    title: "¡Excelente!",
+                    text: "Se guardó correctamente",
+                    icon: "success"
+                });
+                limpiarFormulario();
+            },
+            error: function (error) {
+                Swal.fire("Error", "Error al guardar, " + error.responseText, "error");
+            },
+        });
+  
+    } else {
+        Swal.fire({
+            title: "¡Error!",
+            text: "Llene todos los campos correctamente",
+            icon: "error"
+        });
+    }
+  
+  }
+  
+ // Función para validar el número de documento
 function validarNumero_documento(elemento) {
   var valor = elemento.value;
   if (valor.length < 5) {
@@ -78,73 +149,24 @@ function validarConfirmarContrasena(elemento) {
       });
   }
 }
-
-// Función para crear la cuenta
-function crearCuenta() {
-  let formData = {
-      "tipo_documento": document.getElementById("tipo_documento").value,
-      "numero_documento": document.getElementById("numero_documento").value,
-      "nombre_completo": document.getElementById("nombre_completo").value,
-      "telefono": document.getElementById("telefono").value,
-      "correo": document.getElementById("correo").value,
-      "contrasena": document.getElementById("contrasena").value,
-      "confirmar_contrasena": document.getElementById("confirmar_contrasena").value,
-      "rol": document.getElementById("rol").value
-  };
-
-  let camposValidos = true;
-  let camposRequeridos = [
-      "tipo_documento",
-      "numero_documento",
-      "nombre_completo",
-      "telefono",
-      "correo",
-      "contrasena",
-      "confirmar_contrasena",
-      "rol"
-  ];
-
-  camposRequeridos.forEach(function(campo) {
-      let valorCampo = document.getElementById(campo).value.trim();
-      if (valorCampo === "") {
-          camposValidos = false;
-          return false; // Terminar la iteración si se encuentra un campo vacío
-      }
-  });
-
-  if (camposValidos) {
-      $.ajax({
-          url: url,
-          type: "POST",
-          contentType: "application/json",
-          data: JSON.stringify(formData),
-          success: function(result) {
-              Swal.fire({
-                  title: "¡Excelente!",
-                  text: "Se guardó correctamente",
-                  icon: "success"
-              }).then((result) => {
-                  if (result.isConfirmed) {
-                      limpiarFormulario();
-                  }
-              });
-          },
-          error: function(error) {
-              Swal.fire({
-                  title: "Error",
-                  text: "Error al guardar, " + error.responseText,
-                  icon: "error"
-              });
-          },
-      });
-
-  } else {
-      Swal.fire({
-          title: "¡Error!",
-          text: "Llene todos los campos correctamente",
-          icon: "error"
-      });
+  
+  function limpiarFormulario() {
+    document.getElementById("tipo_documento").className="form-control";
+    document.getElementById("rol").className="form-control";
+    document.getElementById("contrasena").className="form-control";
+    document.getElementById("telefono").className="form-control";
+    document.getElementById("numero_documento").className="form-control";
+    document.getElementById("nombre_completo").className="form-control";
+    document.getElementById("confirmar_contrasena").className="form-control";
+    document.getElementById("correo_electronico").className="form-control";
+  
+  
+    document.getElementById("tipo_documento").value = "";
+    document.getElementById("rol").value = "";
+    document.getElementById("contrasena").value = "";
+    document.getElementById("telefono").value = "";
+    document.getElementById("numero_documento").value = "";
+    document.getElementById("nombre_completo").value = "";
+    document.getElementById("confirmar_contrasena").value = "";
+    document.getElementById("correo_electronico").value = "";
   }
-}
-
-
