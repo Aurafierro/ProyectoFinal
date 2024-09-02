a31d5d13b8fe6fe430d60ebdc4c6598375d5bf18
Juan Pablo <betancourtgomezjuanpablo@gmail.com>
2024-09-02 17:29:50 -0500

    footer del resto de paginas

--- HtmlYCss/js/inicioSesion.js
+++ HtmlYCss/js/inicioSesion.js
@@ -3,13 +3,20 @@ function rearrangeElements() {
   var formContainer = document.getElementById('form-container');
   var imageContainer = document.getElementById('image-container');
 
-  formContainer.style.transform = 'translateX(80%)';
-  imageContainer.style.transform = 'translateX(-125%)';
+  // Verificar si la pantalla es grande (mayor que 768px)
+  if (window.innerWidth > 768) {
+    formContainer.style.transform = 'translateX(80%)';
+    imageContainer.style.transform = 'translateX(-125%)';
 
-  setTimeout(() => {
-      formContainer.style.transform = 'translateX(1)';
-      imageContainer.style.transform = 'translateX(1)';
-  }, 800);
+    setTimeout(() => {
+        formContainer.style.transform = 'translateX(0)'; // Cambiado de 1 a 0 para que vuelva a su posición original
+        imageContainer.style.transform = 'translateX(0)'; // Cambiado de 1 a 0 para que vuelva a su posición original
+    }, 800);
+  } else {
+    // En pantallas pequeñas, asegúrate de que la transformación esté estática
+    formContainer.style.transform = 'translateX(0)';
+    imageContainer.style.transform = 'translateX(0)';
+  }
 }
 
 // Llama a la función después de 1 segundo de que la página haya cargado
