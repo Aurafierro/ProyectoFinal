4ffae57ba83166e5d89bfe85ae90d38de478b149
Aurafierro <auramariafierrofierro@gmail.com>
2024-09-04 22:42:06 -0500

    modificaciones en el fron end

--- HtmlYCss/js/RecuperarContraseña.js
+++ HtmlYCss/js/RecuperarContraseña.js
@@ -1,16 +1,3 @@
-function rearrangeElements() {
-    var container = document.querySelector('.container');
-    var formContainer = document.getElementById('form-container');
-    var imageContainer = document.getElementById('image-container');
-
-    formContainer.style.transform = 'translateX(80%)';
-    imageContainer.style.transform = 'translateX(-125%)';
-
-    setTimeout(() => {
-        formContainer.style.transform = 'translateX(1)';
-        imageContainer.style.transform = 'translateX(1)';
-    }, 800);
-}
 
 // Llama a la función después de 1 segundo de que la página haya cargado
 window.onload = function() {
