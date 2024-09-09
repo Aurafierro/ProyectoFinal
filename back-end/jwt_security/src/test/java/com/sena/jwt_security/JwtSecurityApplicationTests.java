56e049bd51870cad69ff6eab5fa717206ae66975
Sebastian Penna <sebastianpennaariss@gmail.com>
2024-09-09 17:50:33 -0500

    Se hicieron test en el bacEnd

--- back-end/jwt_security/src/test/java/com/sena/jwt_security/JwtSecurityApplicationTests.java
+++ back-end/jwt_security/src/test/java/com/sena/jwt_security/JwtSecurityApplicationTests.java
@@ -1,13 +1,56 @@
 package com.sena.jwt_security;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.sena.jwt_security.models.rol;
+import com.sena.jwt_security.models.userRegistro;
+import com.sena.jwt_security.service.userService;
+
+
 
 @SpringBootTest
 class JwtSecurityApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private userService data;
 
+    @Test
+    void testUserRegistro() {
+ 
+        userRegistro userRegistro = new userRegistro();
+        userRegistro.setTipo_documento("CC");
+        userRegistro.setNumero_documento("1076503030");
+        userRegistro.setNombre_completo("John Sebastian Penna Arias");
+        userRegistro.setTelefono("3023073134");
+        userRegistro.setUsername("sebastianpennaariss@gmail.com");
+        userRegistro.setRol(rol.Usuario);
+        userRegistro.setPassword("");
+        
+    
+
+        data.save(userRegistro);
+
+       
+       
+    }
+    @Test
+    void testUserRegistroIncorrecto() {
+ 
+        userRegistro userRegistro = new userRegistro();
+        userRegistro.setTipo_documento("CC");
+        userRegistro.setNumero_documento("");
+        userRegistro.setNombre_completo("John Sebastian Penna Arias");
+        userRegistro.setTelefono("3023073134");
+        userRegistro.setUsername("sebastianpennaariss@gmail.com");
+        userRegistro.setRol(rol.Usuario);
+        userRegistro.setPassword("");
+        
+    
+
+        data.save(userRegistro);
 }
+    
+   
+    }
