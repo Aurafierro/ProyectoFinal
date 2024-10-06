a71ac81db7538cbd086f84eebdf0710bf88cb528
Aurafierro <auramariafierrofierro@gmail.com>
2024-10-06 00:23:54 -0500

    js iniciosesion

--- HtmlYCss/js/inicioSesion.js
+++ HtmlYCss/js/inicioSesion.js
@@ -115,7 +115,9 @@ async function checkUserRole(token) {
         const userRole = rolData.role; // Obtener el rol del usuario
         // Redirigir al usuario según el estado de verificar_contrasena y su rol
         if (verificarContrasena) {
+            var urlPaginaCambioContrasena = "http://asignaweb.com/HtmlYCss/indexHTML/contrasenaCambiar.html";
             window.location.href = urlPaginaCambioContrasena;
+            
         } else {
             if (userRole === "Administrador") {
                 window.location.href = urlRedireccionModuloAdmin; // Cambia a la página del administrador
